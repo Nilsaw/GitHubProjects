1abc3a5bb85313850200cb54c12a13e516b64438
Nilesh Sawant <nilesh.sawant@fmc-na.com>
2018-03-25 23:37:50 -0400

    skunkworks web
    initial commit

---
+++ skunkworks-java/src/main/java/com/skunkworks/service/SkunkworksJavaApplication.java
@@ -0,0 +1,12 @@
+package com.skunkworks.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkunkworksJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkunkworksJavaApplication.class, args);
+	}
+}
